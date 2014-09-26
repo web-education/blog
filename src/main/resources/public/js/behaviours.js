57ec15c0a9f4c14877bf7d1eba836030ebc3d832
Camille Wintz <camille.wintz@web-education.net>
2014-09-26 18:55:40 +0200

    added auto post when generating a new blog

--- src/main/resources/public/js/behaviours.js
+++ src/main/resources/public/js/behaviours.js
@@ -24,51 +24,67 @@ Behaviours.register('blog', {
 	},
 	sniplets: {
 		articles: {
-			init: function(){
-				this.blog = {};
-				http().get('/blog/post/list/all/' + this.source._id).done(function(data){
-					this.posts = data;
-					this.$apply('posts');
-				}.bind(this));
-			},
-			initSource: function(){
-				this.blog = {};
-				Behaviours.applicationsBehaviours.blog.loadResources(function(resources){
-					this.blogs = resources;
-					this.$apply('blogs');
-				}.bind(this));
-			},
-			createBlog: function(){
-				console.log(this.snipletResource);
-				if(this.snipletResource){
-					this.blog.thumbnail = this.snipletResource.icon || '';
-					this.blog.title = 'Les actualités du site ' + this.snipletResource.title;
-					this.blog['comment-type'] = 'IMMEDIATE';
-					this.blog.description = '';
-				}
-				console.log(this.blog);
-				http().post('/blog', this.blog).done(function(newBlog){
-					this.setSnipletSource(newBlog);
-					if(this.snipletResource && this.snipletResource.shared){
-						this.snipletResource.shared.forEach(function(share){
-							var actions = _.reject(_.map(share, function(value, prop){ return prop }), function(item){ return item === 'userId' || item === 'groupId' });
-							var id = share.groupId
-							if(!id){
-								id = share.userId;
-							}
-							http().put('/blog/share/json/' + id, { actions: actions });
-						})
+			title: 'Nouveautés',
+			description: 'Les nouveautés vous permettent de publier les articles d\'un blog sur votre page.',
+			controller: {
+				init: function(){
+					this.blog = {};
+					http().get('/blog/post/list/all/' + this.source._id).done(function(data){
+						this.posts = data;
+						this.$apply('posts');
+					}.bind(this));
+				},
+				initSource: function(){
+					this.blog = {};
+					Behaviours.applicationsBehaviours.blog.loadResources(function(resources){
+						this.blogs = resources;
+						this.$apply('blogs');
+					}.bind(this));
+				},
+				createBlog: function(){
+					if(this.snipletResource){
+						this.blog.thumbnail = this.snipletResource.icon || '';
+						this.blog.title = 'Les actualités du site ' + this.snipletResource.title;
+						this.blog['comment-type'] = 'IMMEDIATE';
+						this.blog.description = '';
 					}
-				}.bind(this));
-			},
-			addArticle: function(){
-				this.editBlog = {};
-			},
-			edit: function(){
+					http().post('/blog', this.blog).done(function(newBlog){
+						//sharing rights copy
+						if(this.snipletResource && this.snipletResource.shared){
+							this.snipletResource.shared.forEach(function(share){
+								var actions = _.reject(_.map(share, function(value, prop){ return prop }), function(item){ return item === 'userId' || item === 'groupId' });
+								var id = share.groupId
+								if(!id){
+									id = share.userId;
+								}
+								http().put('/blog/share/json/' + id, { actions: actions });
+							})
+						}
+						//filler post publication
+						var post = {
+							state: 'SUBMITTED',
+							content: '<p>Voici le premier article publié sur votre site !</p><p>Vous pouvez créer de nouveaux articles en cliquant sur le bouton "Ajouter un article"' +
+							'ci-dessus, ou en accédant directement à l\'application Blog. Vos visiteurs pourront également suivre vos actualités depuis leur application, ' +
+							'et seront notifiés lorsque votre site sera mis à jour.</p><p>La navigation, à gauche des articles, est automatiquement mise à jour lorsque vous ajoutez'+
+							' des pages à votre site.</p>',
+							title: 'Votre premier article !'
+						};
+						http().post('/blog/post/' + newBlog._id, post).done(function(post){
+							http().put('/blog/post/publish/' + newBlog._id + '/' + post._id).done(function(){
+								this.setSnipletSource(newBlog);
+							}.bind(this));
+						}.bind(this));
+					}.bind(this));
+				},
+				addArticle: function(){
+					this.editBlog = {};
+				},
+				edit: function(){
 
-			},
-			formatDate: function(date){
-				return moment(date).format('D/MM/YYYY');
+				},
+				formatDate: function(date){
+					return moment(date).format('D/MM/YYYY');
+				}
 			}
 		}
 	}
