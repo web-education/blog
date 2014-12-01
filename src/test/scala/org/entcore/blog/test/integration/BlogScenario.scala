aa8bdfa4e945ca4d06aaecf116215c3e32d3258c
dboissin <damien.boissin@web-education.net>
2014-12-01 11:04:01 +0100

    [Test] #2581 : integration tests

---
+++ src/test/scala/org/entcore/blog/test/integration/BlogScenario.scala
@@ -0,0 +1,75 @@
+package org.entcore.blog.test.integration
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import org.entcore.test.appregistry.Role
+
+object BlogScenario {
+
+	val scn =
+    Role.createAndSetRole("Blog")
+    .exec(http("Login teacher")
+      .post("""/auth/login""")
+      .formParam("""email""", """${teacherLogin}""")
+      .formParam("""password""", """blipblop""")
+      .check(status.is(302)))
+//    .exec(http("Upload blog thumbnail")
+//      .post("/workspace/document?application=blog-newblog&protected=true")
+//    .bodyPart(FileBodyPart("file", "tests/src/test/resources/images/blog.jpg", "image/jpeg"))
+//     // .body(RawFileBody("tests/src/test/resources/images/blog.jpg"))
+////      .bodyPart(RawFileBodyPart("file", "tests/src/test/resources/images/blog.jpg", "image/jpeg"))
+////      .upload("file", "tests/src/test/resources/images/blog.jpg", "image/jpeg")
+//      .check(status.is(201), jsonPath("$.status").is("ok"),
+//        jsonPath("$._id").find.saveAs("blogImgId")))
+  .exec(http("Create blog")
+    .post("/blog")
+    .formParam("title", "Mon premier blog")
+    .formParam("description", "Le blog de la classe")
+    .formParam("thumbnail", "/blog/public/img/blog.png") //"/workspace/document/${blogImgId}")
+    .formParam("comment-type", "NONE")
+    .formParam("publish-type", "RESTRAINT")
+    .check(status.is(200), jsonPath("$._id").find.saveAs("blogId")))
+  .exec(http("Create post")
+    .post("/blog/post/${blogId}")
+    .formParam("title", "Le billet de l'enseignant")
+    .formParam("content", "Lorem ipsum si amet dolor...")
+    .check(status.is(200), jsonPath("$._id").find.saveAs("postId")))
+  .exec(http("Create comment with comment-type: none")
+    .post("/blog/comment/${blogId}/${postId}")
+    .formParam("comment", "Le commentaire qui n'est pas autorisé")
+    .check(status.is(400)))
+  .exec(http("Update blog")
+    .put("/blog/${blogId}")
+    .bodyPart(StringBodyPart("comment-type", "RESTRAINT"))
+    .check(status.is(200)))
+  .exec(http("Create comment with comment-type: restraint")
+    .post("/blog/comment/${blogId}/${postId}")
+    .formParam("comment", "Le commentaire qui est autorisé avec modération")
+    .check(status.is(200)))
+  .exec(http("List blog")
+    .get("/blog/list/all")
+    .check(status.is(200),
+      jsonPath("$[0]._id").find.is("${blogId}")))
+  .exec(http("List push before publish")
+    .get("/blog/post/list/all/${blogId}")
+    .check(status.is(200)))
+  .exec(http("Publish post")
+    .put("/blog/post/publish/${blogId}/${postId}")
+    .check(status.is(200)))
+  .exec(http("List post after publish")
+    .get("/blog/post/list/all/${blogId}")
+    .check(status.is(200)))
+  .exec(http("Get share json")
+    .get("/blog/share/json/${blogId}")
+    .check(status.is(200)))
+//  .exec(http("Share blog with students")
+//    .put("/blog/share/json/${blogId}")
+//    .bodyPart(StringBodyPart("groupId", "${profilGroupIds(1)}"))
+//    .bodyPart(StringBodyPart("actions", "org-entcore-blog-controllers-PostController|comments"))
+//    .bodyPart(StringBodyPart("actions", "org-entcore-blog-controllers-PostController|get"))
+//    .bodyPart(StringBodyPart("actions", "org-entcore-blog-controllers-BlogController|get"))
+//    .bodyPart(StringBodyPart("actions", "org-entcore-blog-controllers-PostController|list"))
+//    .check(status.is(200)))
+
+}
