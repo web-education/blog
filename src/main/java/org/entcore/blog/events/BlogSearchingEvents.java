779f2490c9c4ef8ed54595e72627429a7270479f
dbreyton <david.breyton@atos.net>
2016-07-01 07:44:41 +0200

    Upgrade : use advanced tokenization without stemming in mongo text search

--- src/main/java/org/entcore/blog/events/BlogSearchingEvents.java
+++ src/main/java/org/entcore/blog/events/BlogSearchingEvents.java
@@ -121,8 +121,8 @@ public class BlogSearchingEvents implements SearchingEvents {
 		final int skip = (0 == page) ? -1 : page * limit;
 
 		final QueryBuilder worldsQuery = new QueryBuilder();
-		//Set locale to "none", allows to use simple tokenization with no list of stop words and no stemming (in fact, stemming works only with words)
-		worldsQuery.text(MongoDbSearchService.textSearchedComposition(searchWords), "none");
+		//Set locale to "", allows to use advanced tokenization with no stemming (in fact, stemming works only with words and for a given language)
+		worldsQuery.text(MongoDbSearchService.textSearchedComposition(searchWords), "");
 
 		final QueryBuilder blogQuery = new QueryBuilder().start("blog.$id").in(setIds);
 		final QueryBuilder publishedQuery = new QueryBuilder().start("state").is(PUBLISHED_STATE);
