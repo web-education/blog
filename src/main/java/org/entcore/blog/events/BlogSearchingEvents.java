e22e37fd2cf7de3d423eaa757bbe0a8063aba586
dbreyton <david.breyton@atos.net>
2016-07-08 14:09:03 +0200

    [Fix] : not use language for text operator (no stemming, no stop words)

--- src/main/java/org/entcore/blog/events/BlogSearchingEvents.java
+++ src/main/java/org/entcore/blog/events/BlogSearchingEvents.java
@@ -121,8 +121,7 @@ public class BlogSearchingEvents implements SearchingEvents {
 		final int skip = (0 == page) ? -1 : page * limit;
 
 		final QueryBuilder worldsQuery = new QueryBuilder();
-		//Set locale to "", allows to use advanced tokenization with no stemming (in fact, stemming works only with words and for a given language)
-		worldsQuery.text(MongoDbSearchService.textSearchedComposition(searchWords), "");
+		worldsQuery.text(MongoDbSearchService.textSearchedComposition(searchWords));
 
 		final QueryBuilder blogQuery = new QueryBuilder().start("blog.$id").in(setIds);
 		final QueryBuilder publishedQuery = new QueryBuilder().start("state").is(PUBLISHED_STATE);
