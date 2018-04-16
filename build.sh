5c92ce5bedd1ea7d1b75d67fbb6b73b7c95c7883
dboissin <damien.boissin@web-education.net>
2018-04-16 16:51:57 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -5,32 +5,71 @@ then
   mkdir node_modules
 fi
 
-if [ -z ${USER_UID:+x} ]
-then
-  export USER_UID=1000
-  export GROUP_GID=1000
-fi
-
-if [ "$1" == "clean" ]
-then
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle clean
-fi
-if [ $? -eq 0 ]
-then
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install --no-bin-links && node_modules/gulp/bin/gulp.js build" && docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
-fi
-if [ $? -eq 0 ]
-then
-  if [ "$2" == "publish" ]
-  then
-    if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
+case `uname -s` in
+  MINGW*)
+    USER_UID=1000
+    GROUP_UID=1000
+    ;;
+  *)
+    if [ -z ${USER_UID:+x} ]
     then
-      echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
-      echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
-      echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
-      echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
+      USER_UID=`id -u`
+      GROUP_GID=`id -g`
     fi
-    docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
+esac
+
+clean () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle clean
+}
+
+buildNode () {
+  case `uname -s` in
+    MINGW*)
+      docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install --no-bin-links && node_modules/gulp/bin/gulp.js build"
+      ;;
+    *)
+      docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install && node_modules/gulp/bin/gulp.js build"
+  esac
+}
+
+buildGradle () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
+}
+
+publish () {
+  if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
+  then
+    echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
+    echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
+    echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
+    echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
   fi
-fi
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
+}
+
+for param in "$@"
+do
+  case $param in
+    clean)
+      clean
+      ;;
+    buildNode)
+      buildNode
+      ;;
+    buildGradle)
+      buildGradle
+      ;;
+    install)
+      buildNode && buildGradle
+      ;;
+    publish)
+      publish
+      ;;
+    *)
+      echo "Invalid argument : $param"
+  esac
+  if [ ! $? -eq 0 ]; then
+    exit 1
+  fi
+done
 
